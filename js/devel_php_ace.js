bbbe67af95935908519b389095913cb2d2f7f378
Anders Grendstadbakk <anders.2205@gmail.com>
2015-10-13 21:51:56 +0200

    Add localstorage support to keep last input

--- js/devel_php_ace.js
+++ js/devel_php_ace.js
@@ -1,3 +1,41 @@
+
+var StorageUnit = (function() {
+  var is_supported = function() {
+    try {
+      return 'localStorage' in window && window['localStorage'] !== null;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  var _ = window.StorageUnit = {
+    supported: is_supported(),
+
+    save: function(key, data) {
+      var toSave = {
+        data: data
+      }
+      toSave = JSON.stringify(toSave);
+      localStorage['StorageUnit_' + key] = toSave;
+    },
+    get: function(key) {
+      if (typeof localStorage['StorageUnit_' + key] !== 'undefined') {
+        var restoredData = JSON.parse(localStorage['StorageUnit_' + key]);
+        return restoredData.data;
+      }
+      else {
+        return '';
+      }
+    },
+    remove: function(key) {
+      localStorage.removeItem('StorageUnit_' + key);
+    }
+  };
+
+  return window.StorageUnit;
+})();
+
+
 (function($) {
   Drupal.behaviors.php_ace = {
     attach: function(context, settings) {
@@ -12,9 +50,19 @@
         editor.getSession().setValue(textarea.val());
         editor.getSession().on('change', function(){
           textarea.val(editor.getSession().getValue());
+          if (StorageUnit.supported) {
+            StorageUnit.save('drupal-php-' + id, editor.getSession().getValue());
+          }
         });
         textarea.hide();
+        $('.grippie', textarea.parent()).hide();
+
+        if (StorageUnit.supported) {
+          editor.setValue(StorageUnit.get('drupal-php-' + id));
+          editor.clearSelection();
+        }
       });
     }
   };
+
 })(jQuery);
