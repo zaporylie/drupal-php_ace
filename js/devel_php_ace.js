4fec3c713f8758cc5a34fe9704e63a136b2a0a3a
Anders Grendstadbakk <anders.2205@gmail.com>
2015-10-13 22:48:46 +0200

    Fix more related to coding standards

--- js/devel_php_ace.js
+++ js/devel_php_ace.js
@@ -3,45 +3,44 @@
  * Initialize Ace Editor and localStorage support.
  */
 
-StorageUnit = (function() {
-  var is_supported = function() {
-    try {
-      return 'localStorage' in window && window['localStorage'] !== null;
-    }
-    catch (e) {
-      return false;
-    }
-  };
-
-  var _ = window.StorageUnit = {
-    supported: is_supported(),
-
-    save: function(key, data) {
-      var toSave = {
-        data: data
-      }
-      toSave = JSON.stringify(toSave);
-      localStorage['StorageUnit_' + key] = toSave;
-    },
-    get: function(key) {
-      if (typeof localStorage['StorageUnit_' + key] !== 'undefined') {
-        var restoredData = JSON.parse(localStorage['StorageUnit_' + key]);
-        return restoredData.data;
-      }
-      else {
-        return '';
-      }
-    },
-    remove: function(key) {
-      localStorage.removeItem('StorageUnit_' + key);
-    }
-  };
-
-  return window.StorageUnit;
-})();
-
-
 (function($) {
+  var StorageUnit = (function() {
+    var is_supported = function() {
+      try {
+        return 'localStorage' in window && window['localStorage'] !== null;
+      }
+      catch (e) {
+        return false;
+      }
+    };
+
+    var _ = {
+      supported: is_supported(),
+
+      save: function(key, data) {
+        var toSave = {
+          data: data
+        }
+        toSave = JSON.stringify(toSave);
+        localStorage['StorageUnit_' + key] = toSave;
+      },
+      get: function(key) {
+        if (typeof localStorage['StorageUnit_' + key] !== 'undefined') {
+          var restoredData = JSON.parse(localStorage['StorageUnit_' + key]);
+          return restoredData.data;
+        }
+        else {
+          return '';
+        }
+      },
+      remove: function(key) {
+        localStorage.removeItem('StorageUnit_' + key);
+      }
+    };
+
+    return _;
+  })();
+
   Drupal.behaviors.php_ace = {
     attach: function(context, settings) {
       $('.ace-enabled').each(function(i) {
